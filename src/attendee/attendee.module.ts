8d332d8c310688221923db3519cc9e0c1d0e77df
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-28 22:13:33 +0100

    add dependency injection for attendee module

--- src/attendee/attendee.module.ts
+++ src/attendee/attendee.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AttendeeController } from './attendee.controller';
 import { AttendeeService } from './attendee.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Attendee, AttendeeSchema } from 'src/schema/attendee.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Attendee.name, schema: AttendeeSchema },
+    ]),
+  ],
   controllers: [AttendeeController],
-  providers: [AttendeeService]
+  providers: [AttendeeService],
 })
 export class AttendeeModule {}
