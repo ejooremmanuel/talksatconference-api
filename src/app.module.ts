aa8c4099a4c58055ab1c12f702cf1fcbef5aae91
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-28 21:01:44 +0100

    add generate attendee module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TalkModule } from './talk/talk.module';
 import { ChatModule } from './chat/chat.module';
+import { AttendeeModule } from './attendee/attendee.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ChatModule } from './chat/chat.module';
     MongooseModule.forRoot(process.env.MONGO_URI),
     TalkModule,
     ChatModule,
+    AttendeeModule,
   ],
 })
 export class AppModule {}
