6b74f4af683873351874648bdd4418278d5af134
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-28 21:10:57 +0100

    add service for new talk creation

--- src/talk/talk.service.ts
+++ src/talk/talk.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Attendee } from 'src/schema/attendee.schema';
 import { Talk } from 'src/schema/talk.schema';
+import { TalkDto } from './data/data.request';
 
 @Injectable()
 export class TalkService {
@@ -10,4 +11,18 @@ export class TalkService {
     @InjectModel(Talk.name) private talkModel: Model<Talk>,
     @InjectModel(Attendee.name) private attendeeModel: Model<Attendee>,
   ) {}
+
+  async createTalk(data: TalkDto): Promise<unknown> {
+    try {
+      const createdTalk = new this.talkModel({
+        title: data.title,
+      });
+      return createdTalk.save();
+    } catch (error) {
+      console.log(error);
+      throw new HttpException('server error', 500, {
+        cause: error,
+      });
+    }
+  }
 }
