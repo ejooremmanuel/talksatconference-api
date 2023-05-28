f3210ac2a951e8246507ce527dda722d63cbd5c9
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-28 22:50:51 +0100

    add connect chat gateway with chat service to chat module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Attendee, AttendeeSchema } from 'src/schema/attendee.schema';
+import { Chat, ChatSchema } from 'src/schema/chat.schema';
+import { Talk, TalkSchema } from 'src/schema/talk.schema';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
-  controllers: [ChatController],
-  providers: [ChatService]
+  imports: [
+    MongooseModule.forFeature([{ name: Talk.name, schema: TalkSchema }]),
+    MongooseModule.forFeature([
+      { name: Attendee.name, schema: AttendeeSchema },
+    ]),
+    MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+  ],
+  providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Chat } from 'src/schema/chat.schema';
+import { Talk } from 'src/schema/talk.schema';
+import { Attendee } from 'src/schema/attendee.schema';
+import { ChatDto } from './data/data.request';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(
+    @InjectModel(Chat.name) private readonly chatModel: Model<Chat>,
+    @InjectModel(Talk.name) private readonly talkModel: Model<Talk>,
+    @InjectModel(Attendee.name) private readonly attendeeModel: Model<Attendee>,
+  ) {}
+
+  async getChats(): Promise<Chat[]> {
+    return await this.chatModel.find();
+  }
+
+  async saveChat(chat: ChatDto): Promise<void> {
+    try {
+      const findTalk = await this.talkModel
+        .findById(chat.talk)
+        .populate('attendee');
+
+      if (!findTalk) {
+        throw new HttpException('talk not found', 400);
+      }
+
+      const findAttendeeDetails = await this.attendeeModel.findById(
+        chat.sender,
+      );
+      if (!findAttendeeDetails) {
+        throw new HttpException('attendee not found', 400);
+      }
+
+      const checkIfAttendeeInTalk = findTalk.attendee.find(
+        (it) => it.email === findAttendeeDetails.email,
+      );
+
+      if (!checkIfAttendeeInTalk) {
+        throw new HttpException('attendee is not added to talk', 400);
+      }
+
+      const createdChat = new this.chatModel(chat);
+      await createdChat.save();
+      await this.talkModel.findByIdAndUpdate(chat.talk, {
+        $push: {
+          chat: createdChat,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(`server error:${error.message}`, 500, {
+        cause: error,
+      });
+    }
+  }
+}
