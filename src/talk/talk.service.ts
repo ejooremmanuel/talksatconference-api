a29b1bd5da2d63f615fa5b26747d26058cdad042
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-28 21:09:40 +0100

    add make depencies available for app service

--- src/talk/talk.service.ts
+++ src/talk/talk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Attendee } from 'src/schema/attendee.schema';
+import { Talk } from 'src/schema/talk.schema';
 
 @Injectable()
-export class TalkService {}
+export class TalkService {
+  constructor(
+    @InjectModel(Talk.name) private talkModel: Model<Talk>,
+    @InjectModel(Attendee.name) private attendeeModel: Model<Attendee>,
+  ) {}
+}
