351434af0cece8390b15aa669f6972be7c588033
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-28 21:07:57 +0100

    add dependency injection in talk module

--- src/talk/talk.module.ts
+++ src/talk/talk.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TalkController } from './talk.controller';
 import { TalkService } from './talk.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Attendee, AttendeeSchema } from 'src/schema/attendee.schema';
+import { Talk, TalkSchema } from 'src/schema/talk.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Talk.name, schema: TalkSchema }]),
+    MongooseModule.forFeature([
+      { name: Attendee.name, schema: AttendeeSchema },
+    ]),
+  ],
   controllers: [TalkController],
-  providers: [TalkService]
+  providers: [TalkService],
 })
 export class TalkModule {}
