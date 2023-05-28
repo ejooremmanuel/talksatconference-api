70f5c228ccc5b0fa758c24d380e65261f5f8577c
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-28 22:28:24 +0100

    add controller handler for create attendee

--- src/attendee/attendee.controller.ts
+++ src/attendee/attendee.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AttendeeService } from './attendee.service';
+import { AttendeeDto } from './data/data.request';
+import { Response } from 'express';
 
 @Controller('attendee')
-export class AttendeeController {}
+export class AttendeeController {
+  constructor(private attendeeService: AttendeeService) {}
+  @Post()
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+  })
+  @ApiTags('Add attendee')
+  async createAttendees(
+    @Res() res: Response,
+    @Body() data: AttendeeDto,
+  ): Promise<unknown> {
+    const newAttendee = await this.attendeeService.createAttendee(data);
+    return res.status(HttpStatus.CREATED).json({
+      success: true,
+      data: newAttendee,
+    });
+  }
+}
