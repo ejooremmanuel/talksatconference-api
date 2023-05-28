c2eb7cf94930d1a2a169f9e13d94c2875437a356
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-28 20:47:54 +0100

    add generate talk module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TalkModule } from './talk/talk.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
+    TalkModule,
   ],
 })
 export class AppModule {}
