835fd7080f98240d7b9a474095f02c1a46698113
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-28 21:00:13 +0100

    add generate chat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TalkModule } from './talk/talk.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     TalkModule,
+    ChatModule,
   ],
 })
 export class AppModule {}
