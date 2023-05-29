6f7f2afbcf00806a7b5bcc50d39e26e7ab825dc9
Emmanuel Ejoor <55317332+ejooremmanuel@users.noreply.github.com>
2023-05-29 20:11:47 +0100

    Delete dist directory

--- dist/attendee/attendee.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AttendeeService } from './attendee.service';
-import { AttendeeDto } from './data/data.request';
-import { AttendeeResponse } from './data/data.response';
-export declare class AttendeeController {
-    private attendeeService;
-    constructor(attendeeService: AttendeeService);
-    createAttendees(data: AttendeeDto): Promise<AttendeeResponse>;
-}
