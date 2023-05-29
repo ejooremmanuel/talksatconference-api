cf2fbc6c9f34a7835c7fc17882c354b7a719cf07
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-29 16:27:04 +0100

    connect to db then start server

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import mongoose, { connection, mongo } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,8 +14,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api-docs', app, document);
   const PORT = parseInt(process.env.PORT, 10) || 5500;
-  await app.listen(PORT, async () => {
-    console.log(`server is running on ${await app.getUrl()}`);
+  const client = new mongo.MongoClient(process.env.MONGO_URI);
+
+  client.connect().then(async () => {
+    await app.listen(PORT, async () => {
+      console.log(`server is running on ${await app.getUrl()}`);
+    });
   });
 }
 bootstrap();
