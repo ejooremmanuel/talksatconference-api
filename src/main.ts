dd7649e6750d24476111979d13d00d884efacc90
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2023-05-29 16:35:34 +0100

    add catch error from connection

--- src/main.ts
+++ src/main.ts
@@ -16,10 +16,15 @@ async function bootstrap() {
   const PORT = parseInt(process.env.PORT, 10) || 5500;
   const client = new mongo.MongoClient(process.env.MONGO_URI);
 
-  client.connect().then(async () => {
-    await app.listen(PORT, async () => {
-      console.log(`server is running on ${await app.getUrl()}`);
+  client
+    .connect()
+    .then(async () => {
+      await app.listen(PORT, async () => {
+        console.log(`server is running on ${await app.getUrl()}`);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 }
 bootstrap();
